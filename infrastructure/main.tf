48dae42753ef0fffd0b8a873bf6faa12ce3d5493
Lucca Miorelli <lucca.miorelli@gmail.com>
2023-06-18 14:43:05 -0300

    🚧 Removed IAM session temporarily

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -25,45 +25,40 @@ resource "aws_s3_bucket" "nba_mvp_bucket" {
   bucket = var.s3_bucket_name
 }
 
-# Configure Bucket ACL as private
-resource "aws_s3_bucket_acl" "nba_mvp_bucket_acl" {
-  bucket = aws_s3_bucket.nba_mvp_bucket.id
-  acl = "private"
-}
 
-################################################################################
-#                                    IAM USER                                  #
-################################################################################
+# ################################################################################
+# #                                    IAM USER                                  #
+# ################################################################################
 
 
-# Create an IAM User
-resource "aws_iam_user" "nba_mvp_iam_user" {
-  name = var.iam_user_name
-}
+# # Create an IAM User
+# resource "aws_iam_user" "nba_mvp_iam_user" {
+#   name = var.iam_user_name
+# }
 
-# Give IAM user programatic access
-resource "aws_iam_access_key" "nba_mvp_iam_user_access_key" {
-  user = aws_iam_user.nba_mvp_iam_user.name
-}
+# # Give IAM user programatic access
+# resource "aws_iam_access_key" "nba_mvp_iam_user_access_key" {
+#   user = aws_iam_user.nba_mvp_iam_user.name
+# }
 
-# Create the inline policy
-data "aws_iam_policy_document" "s3_get_put_delete_policy_document" {
-  statement {
-    actions = [
-        "s3:GetObject",
-        "s3:PutObject",
-        "s3:DeleteObject"
-    ]
+# # Create the inline policy
+# data "aws_iam_policy_document" "s3_get_put_delete_policy_document" {
+#   statement {
+#     actions = [
+#         "s3:GetObject",
+#         "s3:PutObject",
+#         "s3:DeleteObject"
+#     ]
 
-    resources = [
-        "${aws_s3_bucket.nba_mvp_bucket.arn}/*"
-    ]
-  }
-}
+#     resources = [
+#         "${aws_s3_bucket.nba_mvp_bucket.arn}/*"
+#     ]
+#   }
+# }
 
-# Attach the policy to IAM user
-resource "aws_iam_user_policy" "nba_mvp_iam_user_policy" {
-  name = "NbaMvpPolicy-S3-Auth"
-  user = aws_iam_user.nba_mvp_iam_user.name
-  policy = data.aws_iam_policy_document.s3_get_put_delete_policy_document.json
-}
+# # Attach the policy to IAM user
+# resource "aws_iam_user_policy" "nba_mvp_iam_user_policy" {
+#   name = "NbaMvpPolicy-S3-Auth"
+#   user = aws_iam_user.nba_mvp_iam_user.name
+#   policy = data.aws_iam_policy_document.s3_get_put_delete_policy_document.json
+# }
