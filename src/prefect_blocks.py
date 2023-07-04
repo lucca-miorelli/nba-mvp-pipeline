8a3ddc4b85abe7dfb3063c29437c351371f613c1
Lucca Miorelli <lucca.miorelli@gmail.com>
2023-07-04 14:41:53 -0300

    ✅ Change GitHub block to master branch

--- src/prefect_blocks.py
+++ src/prefect_blocks.py
@@ -2,7 +2,7 @@ from prefect.filesystems import GitHub
 
 block = GitHub(
     repository="https://github.com/lucca-miorelli/nba-mvp-pipeline/",
-    reference="prefect-pipeline",
+    reference="master",
     subfolder="src/prefect_pipeline.py",
 )
 # block.get_directory("") # specify a subfolder of repo
