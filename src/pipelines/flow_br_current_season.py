a936dfb3c5084afa031da87502e9151c0df50386
Lucca Miorelli <lucca.miorelli@gmail.com>
2023-07-27 15:11:59 -0300

    🔨 Update flow with new tasks

--- src/pipelines/flow_br_current_season.py
+++ src/pipelines/flow_br_current_season.py
@@ -1,20 +1,27 @@
-from prefect import Flow
-from tasks.tasks_br_scraper import get_stats, merge_three_dfs
+from prefect import flow
+from tasks.tasks_br_scraper import get_stats, merge_dfs, add_date_column, load_data
+from datetime import datetime
 
 
-CURRENT_SEASON = "2023"
+CURRENT_SEASON = "2023" # "2022-23"
+CURRENT_DAY    = datetime.now().strftime('%Y_%m_%d')
+BUCKET_NAME    = "nba-mvp-pipeline"
 
 
-@Flow
+@flow
 def scrap_current_season_stats():
     
     df_totals   = get_stats(season=CURRENT_SEASON, info="advanced")
     df_advanced = get_stats(season=CURRENT_SEASON, info="totals")
     df_pergame  = get_stats(season=CURRENT_SEASON, info="per_game")
-
-    merged_df = merge_three_dfs(df_totals, df_advanced, df_pergame)
-
+    
     # test_players_uniqueness(list_t, list_a)
+
+    merged_df = merge_dfs(df_totals, df_advanced, df_pergame)
+
+    df = add_date_column(merged_df, CURRENT_DAY)
+
+    load_data(df, BUCKET_NAME, CURRENT_DAY)
 
 
 if __name__ == "__main__":
