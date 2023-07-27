fc2943c3741c00c8fa7a7834fe39ffc9c7948bd1
Lucca Miorelli <lucca.miorelli@gmail.com>
2023-07-27 15:11:03 -0300

    📝Add tasks description and tags

--- src/pipelines/tasks/tasks_br_scraper.py
+++ src/pipelines/tasks/tasks_br_scraper.py
@@ -4,20 +4,12 @@ import pandas as pd
 import awswrangler as wr
 
 
-# @Task
-# def get_advanced_stats(season:str="2023", info:str=None):
-    
-#     df_stats_totals = nba.get_stats(season="2023", info="totals")
-#     print(df_stats_totals.head())
-
-#     df_stats_advanced = nba.get_stats(season="2023", info="advanced")
-#     print(df_stats_advanced.head())
-
-#     return list(df_stats_totals.Player), list(df_stats_advanced.Player)
-
-
-@Task
-def get_stats(season:str=None, info:str=None):
+@task(
+    name="Get Stats",
+    description="Get stats from basketball-reference.com",
+    tags=["NBA", "Basketball-Reference", "Stats", "Extraction"],
+)
+def get_stats(season:str=None, info:str=None)->pd.DataFrame:
     
     df = nba.get_stats(season=season, info=info)
 
@@ -26,8 +18,12 @@ def get_stats(season:str=None, info:str=None):
 
     return df
 
-@Task
-def merge_three_dfs(df1, df2, df3):
+@task(
+    name="Merge DataFrames",
+    description="Merge totals, per_game, and advanced DataFrames into one.",
+    tags=["NBA", "Basketball-Reference", "Stats", "Transformation"],
+)
+def merge_dfs(df1:pd.DataFrame, df2:pd.DataFrame, df3:pd.DataFrame)->pd.DataFrame:
 
     df = df1.merge(df2, on='Player')
     df = df.merge(df3, on='Player')
@@ -36,8 +32,24 @@ def merge_three_dfs(df1, df2, df3):
 
     return df
 
-@Task
-def test_players_uniqueness(stats_t:list[str], stats_a:list[str]):
+@task(
+
+)
+def add_date_column(df:pd.DataFrame, snapshot_date:str)->pd.DataFrame:
+    
+    df['snapshot_date'] = snapshot_date
+
+    print(df.head())
+
+    return df
+
+
+@task(
+    name="Test Players Uniqueness",
+    description="Test keystring in each dataframe to ensure uniqueness.",
+    tags=["NBA", "Basketball-Reference", "Stats", "Test", "Data Quality"],
+)
+def test_players_uniqueness(stats_t:list[str], stats_a:list[str])->None:
 
     set_t = set(stats_a)
     set_a = set(stats_t)
