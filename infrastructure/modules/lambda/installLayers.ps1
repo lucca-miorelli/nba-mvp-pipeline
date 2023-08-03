30d51c5b54122251216029abec553cac529d5fe0
Lucca Miorelli <lucca.miorelli@gmail.com>
2023-08-03 09:52:29 -0300

    📦 Automate py libs compression to lambda layer

---
+++ infrastructure/modules/lambda/installLayers.ps1
@@ -0,0 +1,31 @@
+$requirementsFile = "infrastructure/modules/lambda/requirements.txt"
+$installDir = "infrastructure/modules/lambda/layers"
+
+# Create the installation directory if it doesn't exist
+if (-not (Test-Path $installDir)) {
+  New-Item -ItemType Directory -Path $installDir | Out-Null
+}
+
+# Read the requirements file and install each library
+Get-Content $requirementsFile | ForEach-Object {
+  $libName = $_.Split("=")[0].Trim()
+  $libDir = Join-Path $installDir $libName
+
+  # Skip the installation if the ZIP file already exists
+  if (Test-Path $zipFile) {
+    Write-Host "Skipping installation of $libName because $zipFile already exists."
+    return
+  }
+
+  # Create the library directory if it doesn't exist
+  if (-not (Test-Path $libDir)) {
+    New-Item -ItemType Directory -Path $libDir | Out-Null
+  }
+
+  # Install the library in the library directory
+  pip install $_ -t (Join-Path $libDir "python")
+
+  # Zip the library directory
+#   $zipFile = Join-Path $installDir "$libName.zip"
+#   Compress-Archive -Path $libDir -DestinationPath $zipFile
+}
