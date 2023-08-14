8b988c9ccef376176b1f5b66261639e4b27dfcb1
Lucca Miorelli <lucca.miorelli@gmail.com>
2023-08-14 20:25:19 -0300

    🎨 Improve lambda code readability

--- infrastructure/modules/lambda/load_db/lambda_function.py
+++ infrastructure/modules/lambda/load_db/lambda_function.py
@@ -1,3 +1,7 @@
+####################################################################
+#                         IMPORT LIBRARIES                         #
+####################################################################
+
 import os
 import psycopg2
 import awswrangler as wr
@@ -5,57 +9,81 @@ from sqlalchemy import create_engine
 import urllib.parse
 import json
 
-# rds settings
+
+####################################################################
+#                     CUSTOM EXCEPTION CLASSES                     #
+####################################################################
+
+class S3ReadError(Exception):
+    """Exception raised when there's an error reading from S3."""
+    pass
+
+class DatabaseWriteError(Exception):
+    """Exception raised when there's an error writing to the database."""
+    pass
+
+
+####################################################################
+#                          GLOBAL VARIABLES                        #
+####################################################################
+
+# Get environment variables
 DB_HOST = os.environ['DB_HOST']
 DB_PORT = os.environ['DB_PORT']
 DB_NAME = os.environ['DB_NAME']
 DB_USERNAME = os.environ['DB_USERNAME']
 DB_PASSWORD = os.environ['DB_PASSWORD']
 
+# Create connection string
+CONN_STR = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+# Database table name and schema
+DB_SCHEMA = 'public'
+DB_TABLE  = 'nba_stats'
+
+
+####################################################################
+#                            MAIN FUNCTION                         #
+####################################################################
 
 def lambda_handler(event, context):
+    """
+    Lambda function handler to read Parquet data from S3 and write to a PostgreSQL database.
+
+    Args:
+        event (dict): Event data from S3 passed to the Lambda function.
+        context (object): Lambda context object.
+
+    Returns:
+        None
+    """
     
     print("Received event: " + json.dumps(event, indent=2) + '\n')
     
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    # Get S3 key from event
+    s3_key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     
     try:
-        df = wr.s3.read_parquet(path=f's3://nba-mvp-pipeline/{key}')
+        # Read Parquet file from S3
+        df = wr.s3.read_parquet(path=f's3://nba-mvp-pipeline/{s3_key}')
     except Exception as e:
-        print(e)
+        raise S3ReadError(f"Error reading from S3: {e}")
     else:
+        print("Read from S3 successfully!")
         print(df.head())
 
-    # connect to Postgresql database
-    con = psycopg2.connect(
-        user=DB_USERNAME,
-        host=DB_HOST,
-        database=DB_NAME,
-        port=DB_PORT,
-        password=DB_PASSWORD
-    )
-    
-    # create cursor
-    cur = con.cursor()
-    
-    # execute query
-    cur.execute("SELECT version();")
-    
-    # fetch results
-    record = cur.fetchone()
-    print("You are connected to - ", record,"\n")
-    
-    # close connection
-    cur.close()
-    con.close()
+    print("Writing to database...")
     
     try:
-        print("Writing to database...")
-        conn_str = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
-        
-        df.to_sql('nba_stats', con=create_engine(conn_str), if_exists='append', index=False, schema='public')
-            
-        print("Done!")
-        
+        # Write to PostgreSQL database
+        df.to_sql(
+            name=DB_TABLE,
+            schema=DB_SCHEMA,
+            con=create_engine(CONN_STR),
+            if_exists='append',
+            index=False
+        )
     except Exception as e:
-        print(f"This is the exception: {e}")
+        raise DatabaseWriteError(f"Error writing to database: {e}")
+    else:
+        print("Wrote to database successfully!")
