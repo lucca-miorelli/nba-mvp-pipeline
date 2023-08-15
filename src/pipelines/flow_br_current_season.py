5ae9504ebc01879ebeb9a5dacee55362c90106ee
Lucca Miorelli <lucca.miorelli@gmail.com>
2023-08-14 22:11:46 -0300

    🎨 Improve prefect Tasks and Flow code format

--- src/pipelines/flow_br_current_season.py
+++ src/pipelines/flow_br_current_season.py
@@ -1,26 +1,45 @@
+#########################################################
+#                IMPORT LIBRARIES                       #
+#########################################################
+
 from prefect import flow
 from tasks.tasks_br_scraper import get_stats, merge_dfs, add_date_column, load_data
 from datetime import datetime
 
+
+#########################################################
+#                 GLOBAL VARIABLES                      #
+#########################################################
 
 CURRENT_SEASON = "2023" # "2022-23"
 CURRENT_DAY    = datetime.now().strftime('%Y_%m_%d')
 BUCKET_NAME    = "nba-mvp-pipeline"
 
 
+#########################################################
+#                   FLOW DEFINITION                     #
+#########################################################
+
 @flow
 def scrap_current_season_stats():
-    
+    # Get player statistics for different types
     df_totals   = get_stats(season=CURRENT_SEASON, info="advanced")
     df_advanced = get_stats(season=CURRENT_SEASON, info="totals")
     df_pergame  = get_stats(season=CURRENT_SEASON, info="per_game")
 
+    # Merge DataFrames
     merged_df = merge_dfs(df_totals, df_advanced, df_pergame)
 
-    df = add_date_column(merged_df, CURRENT_DAY)
+    # Add snapshot date column
+    df_with_date  = add_date_column(merged_df, CURRENT_DAY)
 
-    load_data(df, BUCKET_NAME, CURRENT_DAY)
+    # Load data into S3 bucket
+    load_data(df_with_date, BUCKET_NAME, CURRENT_DAY)
 
+
+#########################################################
+#                       MAIN                            #
+#########################################################
 
 if __name__ == "__main__":
     scrap_current_season_stats()
--- src/pipelines/tasks/tasks_br_scraper.py
+++ src/pipelines/tasks/tasks_br_scraper.py
@@ -1,8 +1,16 @@
+#########################################################
+#                IMPORT LIBRARIES                       #
+#########################################################
+
 from BRScraper import nba
 from prefect import task
 import pandas as pd
 import awswrangler as wr
 
+
+#########################################################
+#                   Get Player Stats                    #
+#########################################################
 
 @task(
     name="Get Stats",
@@ -20,28 +28,37 @@ def get_stats(season: str = "2022-23", info: str = "totals") -> pd.DataFrame:
     Returns:
         pd.DataFrame: A DataFrame containing player statistics.
     """
+
+    # Get player statistics
     df = nba.get_stats(season=season, info=info)
 
-    # Process and rename columns based on selected info
-    if info in ("totals", "per_game"):
-        print(f"Processing {info} stats...")
-        df = df.drop(columns=['Pos', 'Age', 'G', 'Season'])
-        if info == "per_game":
-            print("Processing per_game stats...")
-            df = df.drop(columns=['GS'])
-            df = df.rename(columns={i: f"{i}_per_game" for i in df.columns if i not in ["Player", "Tm"]})
-        elif info == "totals":
-            print("Processing totals stats...")
-            df = df.drop(columns=['MP'])
-            df = df.rename(columns={i: f"{i}_totals" for i in df.columns if i not in ["Player", "Tm"]})
-    elif info == "advanced":
-        print("Processing advanced stats...")
-        df = df.rename(columns={i: f"{i}_advanced" for i in df.columns if i not in ["Player", "Tm", "Pos", "Age", "G", "MP", "Season"]})
+    # Drop unnecessary columns based on selected info
+    columns_to_drop = ['Pos', 'Age', 'G', 'Season']
+    if info == "per_game":
+        columns_to_drop.append('GS')
+    elif info == "totals":
+        columns_to_drop.append('MP')
+    df = df.drop(columns=columns_to_drop)
 
-    # Logging information
+    # Rename columns based on selected info
+    if info == "per_game":
+        new_column_suffix = "_per_game"
+    elif info == "totals":
+        new_column_suffix = "_totals"
+    else:  # For "advanced" info
+        columns_to_exclude = ["Player", "Tm", "Pos", "Age", "G", "MP", "Season"]
+        df = df.rename(columns={i: f"{i}_advanced" for i in df.columns if i not in columns_to_exclude})
+    
+    # Log information
+    print(f"Processing {info} stats...")
     print(f"Season: {season}\nInfo: {info}\nShape: {df.shape}\nColumns: {list(df.columns)}\nHead:\n{df.head()}")
 
     return df
+
+
+#########################################################
+#                Merge DataFrames                       #
+#########################################################
 
 @task(
     name="Merge DataFrames",
@@ -60,10 +77,13 @@ def merge_dfs(df1: pd.DataFrame, df2: pd.DataFrame, df3: pd.DataFrame) -> pd.Dat
     Returns:
         pd.DataFrame: A merged DataFrame.
     """
+
     # Check player uniqueness and DataFrame shapes
     players1 = set(df1['Player'])
     players2 = set(df2['Player'])
     players3 = set(df3['Player'])
+
+
     if players1 == players2 == players3:
         print("All players are the same in each DataFrame.")
     else:
@@ -78,13 +98,18 @@ def merge_dfs(df1: pd.DataFrame, df2: pd.DataFrame, df3: pd.DataFrame) -> pd.Dat
             print(f"df{i} has duplicates.")
 
     # Merge DataFrames
-    df = pd.merge(df1, df2, how="inner", on=["Player", "Tm"])
-    df = pd.merge(df, df3, how="inner", on=["Player", "Tm"])
+    merged_df = pd.merge(df1, df2, how="inner", on=["Player", "Tm"])
+    df = pd.merge(merged_df, df3, how="inner", on=["Player", "Tm"])
 
     # Logging information
     print(f"Shape: {df.shape}\nColumns: {list(df.columns)}\nHead:\n{df.head()}")
 
     return df
+
+
+#########################################################
+#             Add Snapshot Date Column                  #
+#########################################################
 
 @task
 def add_date_column(df: pd.DataFrame, snapshot_date: str) -> pd.DataFrame:
@@ -98,13 +123,18 @@ def add_date_column(df: pd.DataFrame, snapshot_date: str) -> pd.DataFrame:
     Returns:
         pd.DataFrame: The DataFrame with the added snapshot date column.
     """
+    # Add snapshot date column
     df['snapshot_date'] = snapshot_date
 
     # Logging information
-    print(df.head())
+    print(f"Added snapshot_date column with value {snapshot_date}.")
 
     return df
 
+
+#########################################################
+#              Test Players Uniqueness                  #
+#########################################################
 
 @task(
     name="Test Players Uniqueness",
@@ -122,14 +152,24 @@ def test_players_uniqueness(stats_t: list[str], stats_a: list[str]) -> None:
     Returns:
         None
     """
+    # Convert lists to sets for comparison
     set_t = set(stats_t)
     set_a = set(stats_a)
 
-    # Logging information
-    print(set_t == set_a)
-    print(set_t.difference(set_a))
-    print(set_a.difference(set_t))
+    # Check for uniqueness
+    are_equal = set_t == set_a
+    t_unique = set_t.difference(set_a)
+    a_unique = set_a.difference(set_t)
 
+    # Logging information
+    print(f"Are the players unique? {are_equal}")
+    print(f"Players unique to totals: {t_unique}")
+    print(f"Players unique to advanced: {a_unique}")
+
+
+#########################################################
+#            Ingest Data into S3 Bucket                 #
+#########################################################
 
 @task(
     name="Ingest Data",
@@ -148,10 +188,10 @@ def load_data(df: pd.DataFrame, bucket_name: str, current_day: str) -> None:
     Returns:
         None
     """
-    wr.s3.to_parquet(
-        df=df,
-        path=f"s3://{bucket_name}/data/raw/players/{current_day}.parquet"
-    )
+    # Construct the S3 path for saving parquet file
+    s3_path = f"s3://{bucket_name}/data/raw/players/{current_day}.parquet"
+
+    wr.s3.to_parquet(df=df, path=s3_path)
 
     # Logging information
     print("Data saved to S3 bucket.")
